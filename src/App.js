6ee41e17dda740573ab88ded7d8e08e750af649c
Sushmita <sushmitaraj15@gmail.com>
2024-10-13 17:45:21 +0530

    commit_this

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from './components/Navbar';
-import ProductList from './components/ProductList'
+import ProductList from './components/ProductList.js';
+import React,{useState} from "react";
 
 
 function App() {
+  const products=[
+    {
+      price: 99999,
+      name: "IPhone 10S Max",
+      quantity: 0,
+    },
+
+    {
+      price: 99999,
+      name: "Redmi Note 10S  Max",
+      quantity: 0,
+    },
+  ];
+
+  let [productList , setProductList] = useState(products);
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...productList];
+    newProductList[index].quantity++;
+    setProductList(newProductList);
+  };
   return (
     <>
       <Navbar />
-      <ProductList/>
-      <Footer/>
+      <main className='container mt-5'>
+      <ProductList productList={productList}
+      incrementQuantity={incrementQuantity}/>
+      </main>
+      {/* <Footer/> */}
+
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 class Navbar extends React.Component {
-  state = {};
+//   state = {};
   render() {
     return (
       <div>
